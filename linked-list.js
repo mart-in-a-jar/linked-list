e2584732ee9b7f8d451c788a54eb0805fb0bdee3
lnicepei <dmitrii.shaidorov@gmail.com>
2022-08-11 15:30:16 +0300

    Move old notes to test file

--- linked-list.js
+++ linked-list.js
@@ -96,7 +96,6 @@ class LinkedList {
       temp = temp.nextNode;
     }
     string += "(null)";
-    console.log(string);
   }
 
   insertAt(value, index) {
@@ -137,34 +136,5 @@ class LinkedList {
   }
 }
 
-const newList = new LinkedList();
+export { LinkedList, Node };
 
-newList.append("hello");
-newList.prepend("I am the new HEAD");
-newList.prepend("NO, I am the new HEAAD");
-newList.append("LOL i am in the end");
-
-console.log("Size:", newList.size());
-console.log("------------------");
-
-console.log("Head:", newList.head());
-console.log("------------------");
-
-console.log("Tail:", newList.tail());
-console.log("------------------");
-
-console.log("At(2):", newList.at(2));
-console.log("------------------");
-
-newList.pop();
-console.log("------------------");
-
-console.log("Contains('hello')", newList.contains("hello"));
-console.log("------------------");
-
-console.log("Find:", newList.find("hello"));
-console.log("------------------");
-
-newList.insertAt("i was inserted", 1);
-
-newList.removeAt(0);
