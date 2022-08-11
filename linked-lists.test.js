cf029f858d4cf7304b90a8abf9e3b4461658eac9
lnicepei <dmitrii.shaidorov@gmail.com>
2022-08-11 17:06:17 +0300

    Tests are awesome), write test to all the methods

--- linked-lists.test.js
+++ linked-lists.test.js
@@ -1,33 +1,94 @@
 import { LinkedList, Node } from "./linked-list";
 
+let HEAD = new Node("Head");
+const newList = new LinkedList(HEAD);
 
-newList.append("hello");
-newList.prepend("I am the new HEAD");
-newList.prepend("NO, I am the new HEAAD");
-newList.append("LOL i am in the end");
+describe("list methods", () => {
+  it("append() should add notes to the list and get the last note's value", () => {
+    newList.append("I was appended");
+    expect(newList.tail()).toBe("I was appended");
+  });
 
-console.log("Size:", newList.size());
-console.log("------------------");
+  it("append() re-check", () => {
+    newList.append("I am in the end");
+    expect(newList.tail()).toBe("I am in the end");
+  });
 
-console.log("Head:", newList.head());
-console.log("------------------");
+  it("prepend() should insert notes before HEAD and point HEAD at them", () => {
+    newList.prepend("I am the new HEAD");
+    expect(newList.head()).toBe("I am the new HEAD");
+  });
 
-console.log("Tail:", newList.tail());
-console.log("------------------");
+  it("prepend() re-check", () => {
+    newList.prepend("NO, I am the new HEAD");
+    expect(newList.head()).toBe("NO, I am the new HEAD");
+  });
 
-console.log("At(2):", newList.at(2));
-console.log("------------------");
+  it("size() should return the size of the list", () => {
+    expect(newList.size()).toBe(5);
+  });
 
-newList.pop();
-console.log("------------------");
+  it("head() should return HEAD", () => {
+    expect(newList.head()).toBe("NO, I am the new HEAD");
+  });
 
-console.log("Contains('hello')", newList.contains("hello"));
-console.log("------------------");
+  it("head() should return TAIL", () => {
+    expect(newList.tail()).toBe("I am in the end");
+  });
 
-console.log("Find:", newList.find("hello"));
-console.log("------------------");
+  it("at() should return value of the element that has given index", () => {
+    expect(newList.at(2)).toBe("I am the new HEAD");
+  });
 
-newList.insertAt("i was inserted", 1);
+  it("pop() should remove HEAD and assign the following node as HEAD", () => {
+    newList.pop();
+    expect(newList.tail()).toBe("I was appended");
+  });
 
-newList.removeAt(0);
-const newList = new LinkedList();
+  it("contains() should return boolean value. True if the provided value is met within the list, otherwise -- false ", () => {
+    expect(newList.contains("hello")).toBeFalsy();
+  });
+
+  it("contains() re-check", () => {
+    expect(newList.contains("I was appended")).toBeTruthy();
+  });
+
+  it("find() should return index at which the provided value is stored", () => {
+    expect(newList.find("I am the new HEAD")).toBe(1);
+  });
+
+  it("insertAt() should create a new node with provided value and place it to provided position", () => {
+    newList.insertAt("I was inserted", 0);
+    expect(newList.find("I was inserted")).toBe(0);
+  });
+
+  it("toString() should display all newList nodes", () => {
+    expect(newList.toString()).toBe(
+      "(I was inserted) -> (NO, I am the new HEAD) -> (I am the new HEAD) -> (Head) -> (I was appended) -> (null)"
+    );
+  });
+
+  it("removeAt() should remove the node with provided value", () => {
+    expect(newList.removeAt(0)).toBe("I was inserted");
+  });
+
+  it("removeAt() should remove the node with provided value", () => {
+    expect(newList.removeAt(0)).toBe("NO, I am the new HEAD");
+  });
+
+  it("removeAt() should remove the node with provided value", () => {
+    expect(newList.removeAt(0)).toBe("I am the new HEAD");
+  });
+
+  it("toString() re-check", () => {
+    expect(newList.toString()).toBe("(Head) -> (I was appended) -> (null)");
+  });
+
+  it("toString() re-check", () => {
+    newList.prepend("The second node");
+    newList.prepend("The first node");
+    expect(newList.toString()).toBe(
+      "(The first node) -> (The second node) -> (Head) -> (I was appended) -> (null)"
+    );
+  });
+});
